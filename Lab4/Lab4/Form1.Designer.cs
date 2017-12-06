21c17ae5c3c7165115b3e478ca2df3ab6f05d960
Platemiy <wobbuffet@bk.ru>
2017-12-06 17:24:51 +0300

    Initial commit

---
+++ Lab4/Lab4/Form1.Designer.cs
@@ -0,0 +1,222 @@
+﻿namespace Lab4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonRead = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textUpload = new System.Windows.Forms.TextBox();
+            this.textCount = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textSearch = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.Search = new System.Windows.Forms.ListBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonRead
+            // 
+            this.buttonRead.Location = new System.Drawing.Point(12, 28);
+            this.buttonRead.Name = "buttonRead";
+            this.buttonRead.Size = new System.Drawing.Size(75, 23);
+            this.buttonRead.TabIndex = 0;
+            this.buttonRead.Text = "Читать";
+            this.buttonRead.UseVisualStyleBackColor = true;
+            this.buttonRead.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.Control;
+            this.label1.Location = new System.Drawing.Point(109, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(163, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Время загрузки и сохранения:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textUpload
+            // 
+            this.textUpload.BackColor = System.Drawing.SystemColors.Window;
+            this.textUpload.Location = new System.Drawing.Point(112, 31);
+            this.textUpload.Name = "textUpload";
+            this.textUpload.ReadOnly = true;
+            this.textUpload.Size = new System.Drawing.Size(127, 20);
+            this.textUpload.TabIndex = 3;
+            // 
+            // textCount
+            // 
+            this.textCount.BackColor = System.Drawing.SystemColors.Window;
+            this.textCount.Location = new System.Drawing.Point(112, 74);
+            this.textCount.Name = "textCount";
+            this.textCount.ReadOnly = true;
+            this.textCount.Size = new System.Drawing.Size(127, 20);
+            this.textCount.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(112, 58);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(71, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Кол-во слов:";
+            this.label2.Click += new System.EventHandler(this.label2_Click_1);
+            // 
+            // textSearch
+            // 
+            this.textSearch.Location = new System.Drawing.Point(112, 100);
+            this.textSearch.Name = "textSearch";
+            this.textSearch.Size = new System.Drawing.Size(127, 20);
+            this.textSearch.TabIndex = 6;
+            this.textSearch.TextChanged += new System.EventHandler(this.textSearch_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 100);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(87, 23);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Поиск строки";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // Search
+            // 
+            this.Search.FormattingEnabled = true;
+            this.Search.Location = new System.Drawing.Point(12, 222);
+            this.Search.Name = "Search";
+            this.Search.Size = new System.Drawing.Size(120, 173);
+            this.Search.TabIndex = 8;
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.SystemColors.Window;
+            this.textBox2.Location = new System.Drawing.Point(138, 222);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 9;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(135, 206);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(82, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Время поиска:";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 157);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(87, 36);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "поиск строки (Л-н)";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.BackColor = System.Drawing.SystemColors.Window;
+            this.textBox3.Location = new System.Drawing.Point(166, 160);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(73, 20);
+            this.textBox3.TabIndex = 13;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(102, 144);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(66, 13);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "расстояние";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(106, 161);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(54, 20);
+            this.numericUpDown1.TabIndex = 15;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 411);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.Search);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textSearch);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textCount);
+            this.Controls.Add(this.textUpload);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonRead);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonRead;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textUpload;
+        private System.Windows.Forms.TextBox textCount;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textSearch;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox Search;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+    }
+}
+
---
+++ Lab4/Lab4/Form1.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+
+namespace Lab4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        List<string> list = new List<string>();
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog fd = new OpenFileDialog();
+            fd.Filter = "текстовые файлы|*.txt";
+            if (fd.ShowDialog() == DialogResult.OK)
+            {
+                Stopwatch t = new Stopwatch();
+                t.Start();
+                //Чтение файла в виде строки
+                string text = File.ReadAllText(fd.FileName);
+                //Разделительные символы для чтения из файла
+                char[] separators = new char[] { ' ', '.', ',', '!', '?', '/', '\t', '\n' };
+                string[] textArray = text.Split(separators);
+             
+            foreach (string strTemp in textArray)
+                {
+                    string str = strTemp.Trim();
+                    if (!list.Contains(str)) list.Add(str);
+                }
+                t.Stop();
+                textUpload.Text = t.Elapsed.ToString();
+                textCount.Text = list.Count.ToString();
+                
+            }
+            else
+            {
+                MessageBox.Show("Необходимо выбрать файл");
+            }
+
+           
+        }
+ 
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            string word = textSearch.Text.Trim();
+            //Если слово для поиска не пусто
+            if (!string.IsNullOrWhiteSpace(word) && list.Count > 0)
+            {
+                //Слово для поиска в верхнем регистре
+                string wordUpper = word.ToUpper();
+                //Временные результаты поиска
+                List<string> tempList = new List<string>();
+                Stopwatch t = new Stopwatch();
+                t.Start();
+                foreach (string str in list)
+                {
+                    if (lab5.Class1.Distance(str,wordUpper)==0)
+                    {
+                        tempList.Add(str);
+                    }
+                }
+                t.Stop();
+                textBox2.Text = t.Elapsed.ToString();
+                Search.BeginUpdate();
+                foreach (string str in tempList)
+                {
+                    Search.Items.Add(str);
+                }
+                Search.EndUpdate();
+            }
+            else
+            {
+                MessageBox.Show("Необходимо выбрать файл и ввести слово для поиска");
+            }
+        }
+
+        private void textSearch_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string word = textBox3.Text.Trim();
+            //Если слово для поиска не пусто
+            if (!string.IsNullOrWhiteSpace(word) && list.Count > 0)
+            {
+                //Слово для поиска в верхнем регистре
+                string wordUpper = word.ToUpper();
+                //Временные результаты поиска
+                List<string> tempList = new List<string>();
+                Stopwatch t = new Stopwatch();
+                t.Start();
+                foreach (string str in list)
+                {
+                    if (lab5.Class1.Distance(str, wordUpper) <= numericUpDown1.Value)
+                    {
+                        tempList.Add(str);
+                    }
+                }
+                t.Stop();
+                textBox2.Text = t.Elapsed.ToString();
+                Search.BeginUpdate();
+                foreach (string str in tempList)
+                {
+                    Search.Items.Add(str);
+                }
+                Search.EndUpdate();
+            }
+            else
+            {
+                MessageBox.Show("Необходимо выбрать файл и ввести слово для поиска");
+            }
+        }
+    }
+}
